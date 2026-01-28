244739e423fb2ff6fea1e436d12111753ba974fa
GaoTong <gao_tong@outlook.com>
2026-01-28 23:16:09 +0800

    temp30

---
+++ src/main/java/com/nowcoder/program/MyProgram.java
@@ -0,0 +1,94 @@
+package com.nowcoder.program;
+
+/**
+ * 编程题
+ */
+public class MyProgram {
+
+    /**
+     * 两数之和
+     * https://leetcode.cn/problems/two-sum/
+     */
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+
+    /**
+     * IP地址无效化
+     * https://leetcode.cn/problems/defanging-an-ip-address/description/
+     */
+    public String defangIPaddr(String address) {
+        return "";
+    }
+
+    /**
+     * 反转字符串
+     * https://leetcode.cn/problems/reverse-string/description/
+     */
+    public void reverseString(char[] s) {
+
+    }
+
+    /**
+     * 字符串中的单词反转
+     * https://leetcode.cn/problems/fan-zhuan-dan-ci-shun-xu-lcof/description/
+     */
+    public String reverseMessage(String message) {
+        return "";
+    }
+
+    /**
+     * 验证回文串
+     * https://leetcode.cn/problems/valid-palindrome/description/
+     */
+    public boolean isPalindrome(String s) {
+        return false;
+    }
+
+    /**
+     * 回文数
+     * https://leetcode.cn/problems/palindrome-number/description/
+     */
+    public boolean isPalindrome(int x) {
+        return false;
+    }
+
+    /**
+     * 最后一个单词的长度
+     * https://leetcode.cn/problems/length-of-last-word/description/
+     */
+    public int lengthOfLastWord(String s) {
+        return 0;
+    }
+
+    /**
+     * 删除有序数组中的重复项
+     * https://leetcode.cn/problems/remove-duplicates-from-sorted-array/description/
+     */
+    public int removeDuplicates(int[] nums) {
+        return 0;
+    }
+
+    /**
+     * 把字符串转换成整数
+     * https://leetcode.cn/problems/ba-zi-fu-chuan-zhuan-huan-cheng-zheng-shu-lcof/description/
+     */
+    public int myAtoi(String str) {
+        return 0;
+    }
+
+    /**
+     * 替换空格
+     */
+
+    /**
+     * 左旋转字符串
+     * 旋转字符串
+     * https://leetcode.cn/problems/rotate-string/
+     */
+    public boolean rotateString(String s, String goal) {
+        return false;
+    }
+
+
+}
---
+++ src/main/java/com/nowcoder/regularity/MyRegularity.java
@@ -0,0 +1,90 @@
+package com.nowcoder.regularity;
+
+import java.util.List;
+
+/**
+ * 规律
+ */
+public class MyRegularity {
+
+    /**
+     * 零矩阵
+     * https://leetcode.cn/problems/zero-matrix-lcci/description/
+     */
+    public void setZeroes(int[][] matrix) {
+
+    }
+
+    /**
+     * 扑克牌中的顺子
+     */
+
+    /**
+     * 跳水板
+     * https://leetcode.cn/problems/diving-board-lcci/description/
+     */
+    public int[] divingBoard(int shorter, int longer, int k) {
+        return null;
+    }
+
+    /**
+     * 一次编辑
+     * https://leetcode.cn/problems/one-away-lcci/description/
+     */
+    public boolean oneEditAway(String first, String second) {
+        return false;
+    }
+
+    /**
+     * 珠玑妙算
+     * https://leetcode.cn/problems/master-mind-lcci/description/
+     */
+    public int[] masterMind(String solution, String guess) {
+        return null;
+    }
+
+    /**
+     * 井字游戏
+     * https://leetcode.cn/problems/tic-tac-toe-lcci/description/
+     */
+    public String tictactoe(String[] board) {
+        return "";
+    }
+
+    /**
+     * 跳跃游戏
+     * https://leetcode.cn/problems/jump-game/description/
+     */
+    public boolean canJump(int[] nums) {
+        return false;
+    }
+
+    /**
+     * 旋转图像
+     * https://leetcode.cn/problems/rotate-image/
+     */
+    public void rotate(int[][] matrix) {
+
+    }
+
+    /**
+     * 螺旋矩阵
+     * https://leetcode.cn/problems/spiral-matrix/description/
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        return null;
+    }
+
+    /**
+     * 搜索二维矩阵II
+     * https://leetcode.cn/problems/search-a-2d-matrix-ii/description/
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        return false;
+    }
+
+    /**
+     *
+     */
+
+}
