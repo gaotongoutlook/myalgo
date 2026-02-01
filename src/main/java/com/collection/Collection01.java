618ea58bccfebab5c4d5cc389e72cbce922e6823
GaoTong <gao_tong@outlook.com>
2026-02-01 22:00:57 +0800

    temp33

---
+++ src/main/java/com/collection/Collection01.java
@@ -0,0 +1,196 @@
+package com.collection;
+
+import org.example.pojo.ListNode;
+import org.example.pojo.TreeNode;
+import java.util.List;
+
+/**
+ * 收藏01
+ * 两数之和
+ * 翻转链表
+ * 两数相加
+ * 无重复子串的最长子串
+ * 有效的括号
+ * 最长回文子串
+ * 删除链表的倒数第N个节点
+ * 买卖股票的最佳时机
+ * 爬楼梯
+ * 最大子序和
+ * 合并两个有序链表
+ * 移动零
+ * 在排序数组中查找元素的第一个和最后一个位置
+ * 盛水最多的容器
+ * 电话号码的字母组合
+ * 三数之和
+ * 环形链表
+ * 二叉树的最大深度
+ * 括号生成
+ * 二叉树的层次遍历
+ * 打家劫舍
+ * 全排列
+ */
+public class Collection01 {
+
+    /**
+     * 两数之和
+     */
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+
+    /**
+     * 翻转链表
+     */
+    public ListNode reverseList(ListNode head) {
+        return null;
+    }
+
+    /**
+     * 无重复子串的最长子串
+     */
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+
+    /**
+     * 两数相加
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+
+    /**
+     * 有效的括号
+     */
+    public boolean isValid(String s) {
+        return false;
+    }
+
+    /**
+     * 最长回文子串
+     */
+    public String longestPalindrome(String s) {
+        return null;
+    }
+
+    /**
+     * 买卖股票的最佳时机
+     */
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+
+    /**
+     * 爬楼梯
+     */
+    public int climbStairs(int n) {
+        return 0;
+    }
+
+    /**
+     * 最大子序和
+     */
+    public int maxSubArray(int[] nums) {
+        return 0;
+    }
+
+    /**
+     * 删除链表的倒数第N个节点
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        return null;
+    }
+
+    /**
+     * 合并两个有序链表
+     */
+    public ListNode mergeTowLists(ListNode l1, ListNode l2) {
+        return null;
+    }
+
+    /**
+     * 移动零
+     */
+    public void moveZeroes(int[] nums) {
+
+    }
+
+    /**
+     * 在排序数组中查找元素的第一个和最后一个位置
+     */
+    public int[] searchRange(int[] nums, int target) {
+        return null;
+    }
+
+    /**
+     * 盛水最多的容器
+     */
+    public int maxArea(int[] height) {
+        return 0;
+    }
+
+    /**
+     * 电话号码的字母组合
+     */
+    public List<String> letterCombinations(String digits) {
+        return null;
+    }
+
+    /**
+     * 三数之和
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        return null;
+    }
+
+    /**
+     * 环形链表
+     */
+    public boolean hasCycle(ListNode head) {
+        return false;
+    }
+
+    /**
+     * 二叉树的最大深度
+     */
+    public int maxDepth(TreeNode root) {
+        return 0;
+    }
+
+    /**
+     * 括号生成
+     */
+    public List<String> generateParenthesis(int n) {
+        return null;
+    }
+
+    /**
+     * 接雨水
+     */
+    public int trap(int[] height) {
+        return 0;
+    }
+
+    /**
+     * 二叉树的层次遍历
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        return null;
+    }
+
+    /**
+     * 打家劫舍
+     */
+    public int rob(int[] nums) {
+        return 0;
+    }
+
+    /**
+     * 全排列
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        return null;
+    }
+
+
+}
